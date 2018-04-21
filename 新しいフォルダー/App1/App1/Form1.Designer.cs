c48c788dd95f8680e83386e84438c0a326871052
PCPC\obata_tatsuya <obata_tatsuya@ict-o.com>
2018-04-21 14:02:37 +0900

    aaa

---
+++ 新しいフォルダー/App1/App1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace App1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnl.Controls.Add(this.btnCalc);
+            this.pnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl.Location = new System.Drawing.Point(0, 0);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(200, 556);
+            this.pnl.TabIndex = 0;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(200, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(700, 556);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCalc.Location = new System.Drawing.Point(0, 0);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(200, 78);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "電卓";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 556);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ 新しいフォルダー/App1/App1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            btnCalc.Click += ((s, e) => {
+                var calc = new WindowsFormsApplication1.Form1();
+                calc.TopLevel = false;
+                //this.Controls.Add(calc);
+                this.pnlMain.Controls.Add(calc);
+                calc.Show();
+                calc.BringToFront();
+            });
+        }
+
+        //private void btnCalc_Click (object sender, EventArgs e) {
+        //    var calc = new WindowsFormsApplication1.Form1();
+        //    calc.TopLevel = false;
+        //    //this.Controls.Add(calc);
+        //    this.pnlMain.Controls.Add(calc);
+        //    calc.Show();
+        //    calc.BringToFront();
+        //}
+    }
+}
